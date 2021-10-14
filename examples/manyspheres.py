fdaf96f4ea342dd80c5314aacf1cef3253a9fccd
David Gauffin Dahlin <davidgd@UG.KTH.SE>
2021-10-14 23:14:46 +0200

    More pictures

--- examples/manyspheres.py
+++ examples/manyspheres.py
@@ -8,8 +8,8 @@ from material import Material, Chesspattern
 WIDTH = 2560
 HEIGHT = 1440
 
-RENDERED_IMAGE = "twospheres.ppm"
-CAMERA = Vector(0, -0.35, -1)
+RENDERED_IMAGE = "manyspheres.ppm"
+CAMERA = Vector(0, -0.3, -1.5)
 OBJECTS = [
     # Ground Plane
     Sphere(
@@ -22,16 +22,57 @@ OBJECTS = [
             reflection=0.2,
         ),
     ),
-    # Blue ball
-
-    # Pink ball
-    # Sphere(Point(-0.75, -0.1, 2.25), 0.6, Material(Color.from_hex("#803980"))),
 ]
-for i in range(500):
-    OBJECTS.append(Sphere(Point(0.002 * i - 2, -0.1, 1 * i  + 2), 0.6, Material(Color.from_hex(f"#0{int(i)}0{int(i)}FF"))))
-    OBJECTS.append(Sphere(Point(0.002 * -i + 2, -0.1, 1 * i + 2), 0.6, Material(Color.from_hex(f"#F{int(i*10)}F{int(i*10)}FF"))))
+
+colors = ["#9932cc",
+          "#4285f4",
+          "#ea4335",
+          "#83c165",
+          "#4697e1",
+          "#81c97f",
+          "#1c72e9",
+          "#4cb942",
+          "#5274fa",
+          "#d5232f",
+          "#84c188",
+          "#0d0e10",
+          "#f44336",
+          "#84c088",
+          "#d37b7a",
+          "#8bc34a",
+          "#89ac76",
+          "#8097bf",
+          "#f2a56f",
+          "#7f89ce",
+          "#80b090",
+          "#ce4741",
+          "#fd3333",
+          "#f08080",
+          "#b0e0e6",
+          "#97857d",
+          "#4ca3dd",
+          "#c0d6e4",
+          "#b0e0e6",
+          "#6897bb",
+          "#00ced1",
+          "#f5f5dc",
+          "#b4eeb4",
+          "#caff70",
+          "#05eaff",
+          "#838b8b",
+          "#ffb6c1",
+          "#ffd859",
+          "#00b3ff",
+          "#ff2301",
+          "#05eaff",
+          "#96abff"]
+for i in range(30):
+    OBJECTS.append(Sphere(Point(0.002 * i - 2, -0.1, 2.5 * i + 3), 0.6,
+                          Material(Color.from_hex(colors[i]))))
+    OBJECTS.append(Sphere(Point(0.002 * -i + 2, -0.1, 2.5 * i + 3), 0.6,
+                          Material(Color.from_hex(colors[30 - i]))))
 
 LIGHTS = [
     Light(Point(1.5, -0.5, -10), Color.from_hex("#FFFFFF")),
     Light(Point(-0.5, -10.5, 0), Color.from_hex("#E6E6E6")),
-    ]
+]
